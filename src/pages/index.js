f40311b197fe65b2b5f1572206290f63428bcac2
rupesh00mahat <rupesh00mahat@gmail.com>
2024-05-22 16:21:12 +0545

    add components and made initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import * as React from "react"
+import Navbar from "../components/header/navbar"
+import Header from "../components/header/header"
+import OurServices from "../components/our-services/our-services"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Fragment>
+    <Navbar/>
+    <Header/>
+    <OurServices/>
+  </Fragment>
 }

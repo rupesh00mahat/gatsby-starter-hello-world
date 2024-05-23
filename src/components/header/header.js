5bef66091ebbd4a0bbe0896be25f08d09d8f1bc1
saroj magar <sarozmagar99@gmail.com>
2024-05-23 10:51:29 +0545

    header section

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import * as styles from "./header.module.css";
+import { Link } from "gatsby";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={styles.siteHeader}>
+      <div className={styles.wrapper}>
+        <div className="header-left-content">
+          <img  alt="logo image"></img>
+        </div>
+        <div className={styles.headermiddlecontent}>
+          <nav>
+            <ul>
+              <Link to="/"></Link>
+              <Link to="/about">About us</Link>
+              <Link to="/service">Service</Link>
+              <Link to="/blog">Blog</Link>
+              <Link to="/page">Page</Link>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.headerRightContent}>
+          <form>
+          <input type="text" className="search-input" placeholder="Search for anything"></input>
+          </form>
+          <button className={styles.sectionButton}>Get Started</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Navbar from "../components/header/navbar"
 import Header from "../components/header/header"
+import { Fragment } from "react"
 import OurServices from "../components/our-services/our-services"
 
 export default function Home() {

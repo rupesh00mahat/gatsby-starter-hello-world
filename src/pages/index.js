399b0a50fcca0215b2bdde8a8939348ff8cf60f4
saroj magar <sarozmagar99@gmail.com>
2024-05-24 09:57:37 +0545

    header section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import  React, {Fragment} from "react"
 import Navbar from "../components/header/navbar"
 import Header from "../components/header/header"
-import { Fragment } from "react"
 import OurServices from "../components/our-services/our-services"
 import '../css/index.css';
 
